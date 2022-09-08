645e296de9c03d57a5fe574d7a4d850385e4a33d
DanilAlekseevich <trofimov.danil.alekseevich@gmail.com>
2022-09-08 07:24:44 +0400

    Add models for api response

---
+++ src/Model/BookCategoryListItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model;
+
+class BookCategoryListItem
+{
+    public function __construct(
+        private readonly int    $id,
+        private readonly string $title,
+        private readonly string $slug
+    )
+    {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+}
