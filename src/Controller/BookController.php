54bdcf6d0b529624aa3739f654a1f0d99713aedf
DanilAlekseevich <trofimov.danil.alekseevich@gmail.com>
2022-10-10 20:50:35 +0400

    Add open api documentation

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Exception\BookCategoryNotFoundException;
+use App\Model\BookCategoryListResponse;
 use App\Service\BookService;
 use HttpException;
+use Nelmio\ApiDocBundle\Model\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Attributes as OA;
 
 class BookController extends AbstractController
 {
@@ -20,7 +23,12 @@ class BookController extends AbstractController
     /**
      * @throws HttpException
      */
-    #[Route(path: '/api/v1/category{id}/books')]
+    #[Route(path: '/api/v1/category/{id}/books')]
+    #[OA\Response(
+        response: 200,
+        description: 'Return books by category id',
+        content: new Model(type: BookCategoryListResponse::class)
+    )]
     public function booksByCategory(int $id): Response
     {
         try {
